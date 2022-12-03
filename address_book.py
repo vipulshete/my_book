df32d2763b1c5864c8d40f989ddaa6c9327cc6ee
vipulshete <vipulshete724@gmail.com>
2022-12-03 16:11:16 +0530

    [Vipul]Add: multiple address books

--- address_book.py
+++ address_book.py
@@ -19,27 +19,62 @@ class User:
             give user input as privious first and modified last name
             return modifies user details
         '''
-        
-        for user in  dict.items():
-            new_user = (user[1])
-            if new_user["first_name"] == first_name:
+        for key, values in  dict.items():
+            if values["first_name"] == first_name:
                 last_name = input("Enter last name: ")
-                new_user["last_name"] = last_name
-            return user
-              
+                values["last_name"] = last_name
+                return values
+
+    def delete_user(self, first_name, dict):
+        '''
+            edit user last name using  his privious first name
+            give user input as privious first and modified last name
+            return modifies user details
+        '''
+        for key, values in  dict.items():
+            if values["first_name"] == first_name:
+                del dict[key]
+                return dict      
+
+
+class Multiplebook():
+
+    def add_my_book(self,dict, adressbook_name, persons):
+        '''
+            Add all persons in new addressbook 
+            give user input as addressbook name and persons
+            return addressbook name
+        '''
+        if adressbook_name not in dict.keys():
+            new_persons = persons.copy()
+            dict.update({adressbook_name : new_persons})
+            return adressbook_name
+
+    def clear_person_dict(self, dict):
+        persons = dict.clear()
+        return "Clear all present persons sucessfully"
 
 
 if __name__ == "__main__":
 
+    ## objects of class
     user = User()
+    multiplebook = Multiplebook()
 
+    ## BOOKS / Dictionary
+    new_multiple_addbook = {}
     addbook = {1: {'first_name': 'vipul', 'last_name': 'shete'}, 2: {'first_name': 'vaibhav', 'last_name': 'joishi'}}
 
     while True:
 
         print("\n" + "1. To Add user")
         print("2. Edit last name using first name")
-        print("3. view address book")
+        print("3. delete user using first name")
+        print("4. view current address book")
+        print("5. Add current address books")
+        print("6. Clear current all persons")
+        print("7. View all address books")
+
         print("To exit select 0" + "\n")
 
         choice = int(input("Select opctions: "))
@@ -57,8 +92,26 @@ if __name__ == "__main__":
             print(user.edit_user(first_name, addbook))
 
         if choice == 3:
+            ## delete user
+            first_name = input("Enter first name: ")
+            print(user.delete_user(first_name, addbook))
+
+        if choice == 4:
             ## print all dictionary
-            print(addbook)    
+            print(addbook) 
+
+        if choice == 5:
+            ## add new user
+            addbook_name = input("Enter address book name name: ")
+            print(multiplebook.add_my_book(new_multiple_addbook, addbook_name, addbook)) 
+
+        if choice == 6:
+            ## clear all current persons
+            print(multiplebook.clear_person_dict(addbook))
+
+        if choice == 7:
+            ## Print all dictionary
+            print(new_multiple_addbook)
 
         elif choice == 0:
             break
